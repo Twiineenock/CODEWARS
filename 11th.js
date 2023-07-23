0fa9f624fe1f8dd2434f936e926b48addf02e5ee
Twiine <twiineenockfox@gmail.com>
2023-07-23 22:01:20 +0300

    11th codewars solution

---
+++ 11th.js
@@ -0,0 +1,67 @@
+/* DESCRIPTION:
+You are given a string of n lines, each substring being n characters long. For example:
+
+s = "abcd\nefgh\nijkl\nmnop"
+
+We will study the "horizontal" and the "vertical" scaling of this square of strings.
+
+A k-horizontal scaling of a string consists of replicating k times each character of the string (except '\n').
+
+Example: 2-horizontal scaling of s: => "aabbccdd\neeffgghh\niijjkkll\nmmnnoopp"
+A v-vertical scaling of a string consists of replicating v times each part of the squared string.
+
+Example: 2-vertical scaling of s: => "abcd\nabcd\nefgh\nefgh\nijkl\nijkl\nmnop\nmnop"
+Function scale(strng, k, v) will perform a k-horizontal scaling and a v-vertical scaling.
+
+Example: a = "abcd\nefgh\nijkl\nmnop"
+scale(a, 2, 3) --> "aabbccdd\naabbccdd\naabbccdd\neeffgghh\neeffgghh\neeffgghh\niijjkkll\niijjkkll\niijjkkll\nmmnnoopp\nmmnnoopp\nmmnnoopp"
+Printed:
+
+abcd   ----->   aabbccdd
+efgh            aabbccdd
+ijkl            aabbccdd
+mnop            eeffgghh
+                eeffgghh
+                eeffgghh
+                iijjkkll
+                iijjkkll
+                iijjkkll
+                mmnnoopp
+                mmnnoopp
+                mmnnoopp
+Task:
+Write function scale(strng, k, v) k and v will be positive integers. If strng == "" return "". */
+
+
+const Helper = function(k, n) {
+    const SPLIT_IDENTIFIER = '\n';
+  
+    const repeatWord = (word, times, identifier) => {
+      if(word === SPLIT_IDENTIFIER) {
+        return word;
+      }
+      
+      return new Array(times).join('x,').split(',').map(x => word).join(identifier);
+    };
+    
+    const repeat = (word, x, identifier) => {
+      return word.split(identifier).map(char => repeatWord(char, x, identifier)).join(identifier);
+    };
+    
+    return {
+      repeat:function(word) {
+        return repeat(repeat(word, n, SPLIT_IDENTIFIER),k,'');
+      }
+    };
+  }
+  
+  function scale(strng, k, n) {
+    const SPLIT_IDENTIFIER = '\n';
+    const helper = new Helper(k, n);
+    
+    if(strng === '') {
+      return '';
+    }
+    
+    return strng.split(SPLIT_IDENTIFIER).map(helper.repeat).join(SPLIT_IDENTIFIER);
+  }
